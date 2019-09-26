0ddb09efb54278b765784585c679716778509797
AjaniBahri <bahrisaepul33@gmail.com>
2019-09-26 18:27:02 +0700

    add data.json

--- server/models/users.js
+++ server/models/users.js
@@ -1,11 +1,49 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const secret = require('../helper/config');
 
-const usersSchema = new Schema({
-    email: { type: String },
-    password: { type: String },
-    fullname: {type: String},
-    token: { type: String }
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+var jwt = require('jsonwebtoken');
+
+
+const userSchema = new Schema({
+    email: String,
+    password: String,
+    fullname: String,
+    token: String
 });
 
-module.exports = mongoose.model('users', usersSchema);
+userSchema.pre('save', function(next) {
+    let user = this;
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+    bcrypt.hash(user.password, saltRounds, function(err, hash) {
+      if(err) return next(err)
+      user.password = hash;
+      user.token = user.generateToken();
+      next()
+    });
+  });
+  
+  userSchema.methods.comparePassword = function(plainPassword, done) {
+    bcrypt.compare(plainPassword, this.password).then(function(res) {
+      done(res);
+    });
+  };
+  
+  userSchema.methods.generateToken = function() {
+    let user = this;
+    delete user.password;
+    let token = jwt.sign({email: user.email}, secret.secret);
+    console.log(token);
+    return token;
+  };
+  
+  userSchema.statics.decodeToken = function(token) {
+    return jwt.verify(token, secret.secret);
+  };
+
+
+
+module.exports = mongoose.model('users', userSchema);
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,109 +2,36 @@ var express = require('express');
 var router = express.Router();
 const Users = require('../models/users');
 const helper = require('../helper/util');
-const auth = require('../helper/auth')
-var jwt = require('jsonwebtoken');
-
-const bcrypt = require('bcrypt');
-var salt = bcrypt.genSaltSync(10);
-
-router.get('/', (req, res) => {
-  Users.find({}, (err, data) => {
-    if (err) return res.send(err)
-    res.json(data)
-  })
-})
+const auth = require('../helper/auth');
 
 
+// register
 router.post('/register', (req, res) => {
-  let hash = bcrypt.hashSync(`${req.body.password}`, salt);
-  let hashs = bcrypt.hashSync(`${req.body.retypepassword}`, salt)
-  // console.log(hashs)
-
-  if (hash == hashs) {
-    Users.create({
-      email: req.body.email,
-      password: hash,
-      retypepassword: hashs
-    }, function (err, user) {
-      if (err) {
-        res.status(401).json({
-          error: true,
-          message: 'Error'
-        })
-      }
-      let token = helper.token(user._id);
-      user.token = token;
-      user.save((err) => {
-        res.status(200).json({
-          data: { email: user.email },
-          token: token
-        })
-      })
+  const { email, password, retypepassword } = req.body;
+  let response = {
+    status: true,
+    message: '',
+    data: null
+  }
+  if (password == retypepassword) {
+    let user = new Users({ email, password });
+    user.save((err) => {
+      response.message = "create success",
+        response.data = {
+          user,
+          token: user.token
+        }
+      res.json(response)
     })
   } else {
-    res.status(401).json({
-      message: 'Email not Found'
-    })
+    response.status = false,
+      response.message = '',
+      res.json(response);
   }
 });
 
-router.post('/login', (req, res) => {
-  Users.findOne({
-    email: req.body.email
-  })
-    .then(data => {
-      if (!data) {
-        res.status(304).json({
-          error: true,
-          message: 'Email Not Found'
-        })
-      } else {
-        bcrypt.compare(req.body.password, data.password, function (err, respon) {
-          if (err) {
-            res.status(401).json({
-              error: true,
-              message: 'Error login'
-            })
-          } else {
-            if (respon) {
-              res.status(200).json({
-                message: 'login success',
-                data: { email: data.email },
-                token: helper.token(data._id)
-              })
-            } else {
-              res.status(304).json({
-                error: true,
-                message: 'Email & Password Failed'
-              })
-            }
-          }
-        });
-      }
-    }).catch(err => {
-      res.status(401).json({
-        error: true,
-        message: 'Error login'
-      })
-    })
-});
-
-router.post('/check', auth, (req, res) => {
-  res.status(200).json({ valid: 'true' });
-});
-
-router.post('/destroy', (req, res) => {
-  const token = req.headers.authorization;
-
-  Users.findOneAndUpdate({ 'token': token }, { 'token': '' })
-    .then(() => {
-      res.status(200).json({
-        logut: true
-
-      })
-    })
-
-});
+router.login('/login', (req, res) => {
+  const {email, password} =req.body;
+})
 
 module.exports = router;
